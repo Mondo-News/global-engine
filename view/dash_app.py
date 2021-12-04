6bf1b0d2f2aa7ed0f7b73fc95c17f0572ab1b720
GamesluxX <benedikt.stroebl@yahoo.de>
2021-12-04 17:42:04 +0100

    Renaming and deleting files

--- view/dash_app.py
+++ view/dash_app.py
@@ -1,6 +1,6 @@
 import dash
 from controller.controller import controllerObject
-from view.visualization import viewObject
+from view.map_viz import viewObject
 from dash import html, dcc
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
