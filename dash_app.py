ca4b9da8b3af85c7843086b52eeca227dec134f4
GamesluxX <benedikt.stroebl@yahoo.de>
2021-12-06 18:49:25 +0100

    modified refresh call in callback

--- dash_app.py
+++ dash_app.py
@@ -125,10 +125,10 @@ def serve_layout():
                 html.Th(html.P([html.A([html.Img(
                     src="https://github.com/Mondo-News/global-engine/blob/main/assets/github-logo.png?raw=true",
                     width='60px')],
-                                       href="https://github.com/Mondo-News/global-engine", target="__blank")],
-                               style={'text-align': 'left',
-                                      'font-family': 'Corbel',
-                                      'font-size': '12pt'})),
+                    href="https://github.com/Mondo-News/global-engine", target="__blank")],
+                    style={'text-align': 'left',
+                           'font-family': 'Corbel',
+                           'font-size': '12pt'})),
                 html.Th(html.P(["Mondo News",
                                 html.Br(),
                                 "Please reach out via ",
@@ -149,6 +149,133 @@ def serve_layout():
 app.layout = serve_layout()
 
 
+# @app.callback(
+#     dash.dependencies.Output('map', 'srcDoc'),
+#     [dash.dependencies.Input('general', 'n_clicks'),
+#      dash.dependencies.Input('technology', 'n_clicks'),
+#      dash.dependencies.Input('health', 'n_clicks'),
+#      dash.dependencies.Input('science', 'n_clicks'),
+#      dash.dependencies.Input('business', 'n_clicks'),
+#      dash.dependencies.Input('sports', 'n_clicks'),
+#      dash.dependencies.Input('search', 'value')])
+# def update_map(general_n_clicks, technology_n_clicks, health_n_clicks, science_n_clicks, business_n_clicks,
+#                sports_n_clicks, query_string):
+#     print('New keyword search with query: ' + str(query_string))
+#     if query_string == '' or query_string is None:
+#         print("Used method with emtpy query string input")
+#         viewObject.refreshMap('')
+#     else:
+#         print("Used method with query string input")
+#         viewObject.refreshMap(query_string)
+#
+#     if general_n_clicks == technology_n_clicks == health_n_clicks == science_n_clicks == business_n_clicks == sports_n_clicks == 0 and query_string is None:
+#         return dash.no_update
+#     else:
+#         return open(utils.path_map_html_file, 'r').read()
+
+#
+# @app.callback(Output('general', 'style'), [Input('general', 'n_clicks')])
+# def change_button_style(n_clicks):
+#     if (n_clicks % 2) != 0:
+#         return black_button_style
+#     else:
+#         return white_button_style
+#
+#
+# @app.callback(Output('technology', 'style'), [Input('technology', 'n_clicks')])
+# def change_button_style(n_clicks):
+#     if (n_clicks % 2) != 0:
+#         return white_button_style
+#     else:
+#         return black_button_style
+#
+#
+# @app.callback(Output('health', 'style'), [Input('health', 'n_clicks')])
+# def change_button_style(n_clicks):
+#     if (n_clicks % 2) != 0:
+#         return white_button_style
+#     else:
+#         return black_button_style
+#
+#
+# @app.callback(Output('science', 'style'), [Input('science', 'n_clicks')])
+# def change_button_style(n_clicks):
+#     if (n_clicks % 2) != 0:
+#         return white_button_style
+#     else:
+#         return black_button_style
+#
+#
+# @app.callback(Output('business', 'style'), [Input('business', 'n_clicks')])
+# def change_button_style(n_clicks):
+#     if (n_clicks % 2) != 0:
+#         return white_button_style
+#     else:
+#         return black_button_style
+#
+#
+# @app.callback(Output('sports', 'style'), [Input('sports', 'n_clicks')])
+# def change_button_style(n_clicks):
+#     if (n_clicks % 2) != 0:
+#         return white_button_style
+#     else:
+#         return black_button_style
+
+
+# @app.callback(Output('hidden-div1', 'children'), inputs=[Input('general', 'n_clicks'), Input('general', 'id')])
+# def change_categories(n_clicks, id):
+#     if (n_clicks % 2) != 0:
+#         controllerObject.deselectCategory(id)
+#     else:
+#         controllerObject.selectCategory(id)
+#     return id
+#
+#
+# @app.callback(Output('hidden-div2', 'children'), inputs=[Input('technology', 'n_clicks'), Input('technology', 'id')])
+# def change_categories(n_clicks, id):
+#     if (n_clicks % 2) != 0:
+#         controllerObject.selectCategory(id)
+#     else:
+#         controllerObject.deselectCategory(id)
+#     return id
+#
+#
+# @app.callback(Output('hidden-div3', 'children'), inputs=[Input('business', 'n_clicks'), Input('business', 'id')])
+# def change_categories(n_clicks, id):
+#     if (n_clicks % 2) != 0:
+#         controllerObject.selectCategory(id)
+#     else:
+#         controllerObject.deselectCategory(id)
+#     return id
+#
+#
+# @app.callback(Output('hidden-div4', 'children'), inputs=[Input('health', 'n_clicks'), Input('health', 'id')])
+# def change_categories(n_clicks, id):
+#     if (n_clicks % 2) != 0:
+#         controllerObject.selectCategory(id)
+#     else:
+#         controllerObject.deselectCategory(id)
+#     return id
+#
+#
+# @app.callback(Output('hidden-div5', 'children'), inputs=[Input('sports', 'n_clicks'), Input('sports', 'id')])
+# def change_categories(n_clicks, id):
+#     if (n_clicks % 2) != 0:
+#         controllerObject.selectCategory(id)
+#     else:
+#         controllerObject.deselectCategory(id)
+#     return id
+#
+#
+# @app.callback(Output('hidden-div6', 'children'), inputs=[Input('science', 'n_clicks'), Input('science', 'id')])
+# def change_categories(n_clicks, id):
+#     if (n_clicks % 2) != 0:
+#         controllerObject.selectCategory(id)
+#         print('Science Button pressed')
+#     else:
+#         controllerObject.deselectCategory(id)
+#     return id
+
 @app.callback(
     dash.dependencies.Output('map', 'srcDoc'),
     [dash.dependencies.Input('general', 'n_clicks'),
@@ -160,6 +287,18 @@ app.layout = serve_layout()
      dash.dependencies.Input('search', 'value')])
 def update_map(general_n_clicks, technology_n_clicks, health_n_clicks, science_n_clicks, business_n_clicks,
                sports_n_clicks, query_string):
+    ctx = dash.callback_context
+
+    for prop in ctx.triggered:
+        if "search.value" != prop['prop_id']:
+            id = prop['prop_id'][:len(prop['prop_id']) - 9]
+            print("ID: " + id)
+            if prop['value'] is not None:
+                if (prop['value'] % 2) != 0:
+                    controllerObject.selectCategory(id)
+                else:
+                    controllerObject.deselectCategory(id)
+
     print('New keyword search with query: ' + str(query_string))
     if query_string == '' or query_string is None:
         print("Used method with emtpy query string input")
@@ -172,109 +311,6 @@ def update_map(general_n_clicks, technology_n_clicks, health_n_clicks, science_n
         return dash.no_update
     else:
         return open(utils.path_map_html_file, 'r').read()
-
-
-@app.callback(Output('general', 'style'), [Input('general', 'n_clicks')])
-def change_button_style(n_clicks):
-    if (n_clicks % 2) != 0:
-        return black_button_style
-    else:
-        return white_button_style
-
-
-@app.callback(Output('technology', 'style'), [Input('technology', 'n_clicks')])
-def change_button_style(n_clicks):
-    if (n_clicks % 2) != 0:
-        return white_button_style
-    else:
-        return black_button_style
-
-
-@app.callback(Output('health', 'style'), [Input('health', 'n_clicks')])
-def change_button_style(n_clicks):
-    if (n_clicks % 2) != 0:
-        return white_button_style
-    else:
-        return black_button_style
-
-
-@app.callback(Output('science', 'style'), [Input('science', 'n_clicks')])
-def change_button_style(n_clicks):
-    if (n_clicks % 2) != 0:
-        return white_button_style
-    else:
-        return black_button_style
-
-
-@app.callback(Output('business', 'style'), [Input('business', 'n_clicks')])
-def change_button_style(n_clicks):
-    if (n_clicks % 2) != 0:
-        return white_button_style
-    else:
-        return black_button_style
-
-
-@app.callback(Output('sports', 'style'), [Input('sports', 'n_clicks')])
-def change_button_style(n_clicks):
-    if (n_clicks % 2) != 0:
-        return white_button_style
-    else:
-        return black_button_style
-
-
-@app.callback(Output('hidden-div1', 'children'), inputs=[Input('general', 'n_clicks'), Input('general', 'id')])
-def change_categories(n_clicks, id):
-    if (n_clicks % 2) != 0:
-        controllerObject.deselectCategory(id)
-    else:
-        controllerObject.selectCategory(id)
-    return id
-
-
-@app.callback(Output('hidden-div2', 'children'), inputs=[Input('technology', 'n_clicks'), Input('technology', 'id')])
-def change_categories(n_clicks, id):
-    if (n_clicks % 2) != 0:
-        controllerObject.selectCategory(id)
-    else:
-        controllerObject.deselectCategory(id)
-    return id
-
-
-@app.callback(Output('hidden-div3', 'children'), inputs=[Input('business', 'n_clicks'), Input('business', 'id')])
-def change_categories(n_clicks, id):
-    if (n_clicks % 2) != 0:
-        controllerObject.selectCategory(id)
-    else:
-        controllerObject.deselectCategory(id)
-    return id
-
-
-@app.callback(Output('hidden-div4', 'children'), inputs=[Input('health', 'n_clicks'), Input('health', 'id')])
-def change_categories(n_clicks, id):
-    if (n_clicks % 2) != 0:
-        controllerObject.selectCategory(id)
-    else:
-        controllerObject.deselectCategory(id)
-    return id
-
-
-@app.callback(Output('hidden-div5', 'children'), inputs=[Input('sports', 'n_clicks'), Input('sports', 'id')])
-def change_categories(n_clicks, id):
-    if (n_clicks % 2) != 0:
-        controllerObject.selectCategory(id)
-    else:
-        controllerObject.deselectCategory(id)
-    return id
-
-
-@app.callback(Output('hidden-div6', 'children'), inputs=[Input('science', 'n_clicks'), Input('science', 'id')])
-def change_categories(n_clicks, id):
-    if (n_clicks % 2) != 0:
-        controllerObject.selectCategory(id)
-        print('Science Button pressed')
-    else:
-        controllerObject.deselectCategory(id)
-    return id
 
 
 if __name__ == '__main__':
