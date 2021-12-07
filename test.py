18058c3c82ca2b706f73f0136269b0c08f888fd3
GamesluxX <benedikt.stroebl@yahoo.de>
2021-12-07 18:08:39 +0100

    added worker to heroku

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.triggers.cron import CronTrigger
 from controller.controller import controllerObject
 
-sched = BlockingScheduler()
-
-
-def update_data():
-    print("Daily data Update at 9 o'clock started...")
-    print("Daily data update finished successfully!")
-
-
-sched.add_job(update_data, CronTrigger.from_crontab('* * * * *'))
-sched.start()
+print("Daily data Update started...")
+controllerObject.updateArticleData()
+print("Daily data update finished successfully!")
